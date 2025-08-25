ce5fffd15fb6c3e0d5c5be4d36ff8bc1a5b75e56
jair efren tapia romo <jair.tapia1379@alumnos.udg.mx>
2025-08-24 21:51:55 -0600

    feat: initialize JWT authentication routes and apply rate limiting through middleware.

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 const express = require('express');
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const app = express();
 
-const options = {
-  definition: {
+const swaggerOptions = {
+  swaggerDefinition: {
     openapi: "3.0.0",
     info: {
-      title: "API de CUCEIShare",
+      title: "Mi API",
       version: "1.0.0",
-      description: "Documentación de la API con Swagger",
+      description: "API con rutas protegidas usando JWT"
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
     },
   },
-  apis: ["./routes/*.js"], // aquí leerá los comentarios de tus rutas
+  apis: ["./routes/*.js"], // Aquí van tus rutas documentadas
 };
-
-const specs = swaggerJsdoc(options);
+const specs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+const Authlimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minuto
+  max: 5, // máximo 5 peticiones por IP
+  message: {
+    message: "Demasiados intentos, inténtalo de nuevo más tarde"
+  }
+});
+
+const Applimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minuto
+  max: 15, // máximo 15 peticiones por IP
+  message: {
+    message: "Demasiados intentos, inténtalo de nuevo más tarde"
+  }
+});
 
 app.use(cors());
 app.use(express.json());
 
 const apuntesRoutes = require('./routes/apuntes');
-app.use('/apuntes', apuntesRoutes);
+const authRoutes = require('./routes/auth');
+
+app.use('/apuntes', Applimiter, apuntesRoutes);
+app.use('/auth', Authlimiter, authRoutes);
 
 app.get('/', (req, res) => {
   res.send('CUCEIShare API funcionando 🎓');
